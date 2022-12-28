6c0b48d0b69a9ee5808437985494ecfdab5f2bc1
ChoiWonYu <phillip07290@naver.com>
2022-12-29 04:36:04 +0900

    refactor: 모듈 분리

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,12 +1,9 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
 import { UsersModule } from './users/users.module';
-import { EmailService } from './email/email.service';
 
 @Module({
   imports: [UsersModule],
-  controllers: [AppController],
-  providers: [AppService, EmailService],
+  controllers: [],
+  providers: [],
 })
 export class AppModule {}
